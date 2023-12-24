09f2b73a2ff677ac1e1b8c5a80f2e87f744c2d5f
Psc <Psc@DESKTOP-IES4CND>
2023-12-24 08:32:10 -0800

    Code for delete customers

---
+++ CucumberAutomation/src/main/java/pageObjects/DeleteCustomer.java
@@ -0,0 +1,29 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class DeleteCustomer {
+	
+	
+	
+public WebDriver ldriver;
+   
+public DeleteCustomer(WebDriver rdriver) {
+	
+		ldriver = rdriver;
+		PageFactory.initElements(rdriver,this);
+	}
+
+
+public void DeletCustomer() {
+	
+	
+	
+	
+}
+
+
+
+
+}
